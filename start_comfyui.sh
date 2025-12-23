2d4bdd5074a81a5048453a6ead43eb90fe653a0d
MINETA "m10i" Hiroki <h-mineta@0nyx.net>
2025-12-23 23:49:35 +0900

    [fix] Fixed start_comfyui.sh

--- start_comfyui.sh
+++ start_comfyui.sh
@@ -11,6 +11,13 @@ if [ -f env ]; then
   set +a
 fi
 
+# Override with ./.env if it exists
+if [ -f ./.env ]; then
+  set -a
+  source ./.env
+  set +a
+fi
+
 ARGS="--force-fp16"
 # --force-fp32 が引数に含まれている場合、ARGSに"--force-fp32"をセット
 for arg in "$@"; do
@@ -32,5 +39,6 @@ podman run -d --replace \
   --volume "$(pwd)/data:/workspace/data" \
   --volume "$(pwd)/output:/workspace/output" \
   --device "nvidia.com/gpu=all" \
-  -e CLI_ARGS="${ARGS}" \
+  --env ENABLED_COMFYUI_PREVIEW_GALLERY=${ENABLED_COMFYUI_PREVIEW_GALLERY:-"false"} \
+  --env CLI_ARGS="${ARGS}" \
   localhost/comfyui:${COMFYUI_TAG:-"latest"}
