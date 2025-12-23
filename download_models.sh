b208310c3c99a0dc7c88f76a4e76d5e8d7505a36
MINETA "m10i" Hiroki <h-mineta@0nyx.net>
2025-12-24 07:41:46 +0900

    [add] Added FLUX.2 models URL.

--- download_models.sh
+++ download_models.sh
@@ -16,5 +16,7 @@ podman run -it --rm \
   --name downloader \
   --env "ENABLED_WAN2_MODELS_DOWNLOAD=${ENABLED_WAN2_MODELS_DOWNLOAD:-"false"}" \
   --env "ENABLED_WAN2_MODELS_CHECKSUM=${ENABLED_WAN2_MODELS_CHECKSUM:-"false"}" \
+  --env "ENABLED_FLUX2_MODELS_DOWNLOAD=${ENABLED_FLUX2_MODELS_DOWNLOAD:-"false"}" \
+  --env "ENABLED_FLUX2_MODELS_CHECKSUM=${ENABLED_FLUX2_MODELS_CHECKSUM:-"false"}" \
   --volume "$(pwd)/data:/workspace/data" \
   localhost/downloader:latest
--- services/downloader/entrypoint.sh
+++ services/downloader/entrypoint.sh
@@ -12,6 +12,7 @@ export DOWNLOAD_DIR="/workspace/data/models"
 rm -f "${DOWNLOAD_LIST}" >/dev/null 2>&1
 rm -f "${CHECKSUM_LIST}" >/dev/null 2>&1
 
+# Wan2.2 Models
 if [ "${ENABLED_WAN2_MODELS_DOWNLOAD:-"false"}" = "true" ] || [ "${ENABLED_WAN2_MODELS_DOWNLOAD:-"0"}" = "1" ]; then
     echo "WAN2 Models download enabled."
     cat /container/preset_lists/download_wan2.txt >> "${DOWNLOAD_LIST}"
@@ -21,6 +22,17 @@ if [ "${ENABLED_WAN2_MODELS_CHECKSUM:-"false"}" = "true" ] || [ "${ENABLED_WAN2_
     cat /container/preset_lists/checksum_wan2.txt >> "${CHECKSUM_LIST}"
 fi
 
+# FLUX.2 Models
+if [ "${ENABLED_FLUX2_MODELS_DOWNLOAD:-"false"}" = "true" ] || [ "${ENABLED_FLUX2_MODELS_DOWNLOAD:-"0"}" = "1" ]; then
+    echo "FLUX.2 Models download enabled."
+    cat /container/preset_lists/download_flux2.txt >> "${DOWNLOAD_LIST}"
+fi
+if [ "${ENABLED_FLUX2_MODELS_CHECKSUM:-"false"}" = "true" ] || [ "${ENABLED_FLUX2_MODELS_CHECKSUM:-"0"}" = "1" ]; then
+    echo "FLUX.2 Models checksum verification enabled."
+    cat /container/preset_lists/checksum_flux2.txt >> "${CHECKSUM_LIST}"
+fi
+
+# Custom user lists
 if [ -f "${DOWNLOAD_DIR}/download_list.txt" ]; then
     echo "Custom download list found in download directory. Appending to download list."
     cat "${DOWNLOAD_DIR}/download_list.txt" >> "${DOWNLOAD_LIST}"
